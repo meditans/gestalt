f0227498b89ed60eca80af4d9b87ec9ad1c43583
Carlo Nucera <carlo.nucera@protonmail.com>
2020-06-18 05:28:54 -0400

    Add example queries result

--- examples.pl
+++ examples.pl
@@ -32,7 +32,33 @@ e4(X) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %?- gestalt(c(X,Y)).
+%@ user:c(a, b) :-
+%@     user:a(a, b).
+%@ user:c(c, d) :-
+%@     user:a(c, d).
+%@ user:c(c, d) :-
+%@     user:b(c, d).
+%@ true.
 
 %?- gestalt(d(X,Y)).
+%@ user:d(_, _) :-
+%@     user:a(a, b),
+%@     no-(user:c(b, a)).
+%@ user:d(_, _) :-
+%@     user:a(c, d),
+%@     no-(user:c(d, c)).
+%@ true.
 
 %?- gestalt(e4(X)).
+%@ user:e4(_) :-
+%@     user:e1(a),
+%@     user:e2(a),
+%@     no-(user:e3(a)).
+%@ user:e4(_) :-
+%@     user:e1(a),
+%@     user:e2(b),
+%@     no-(user:e3(b)).
+%@ user:e4(_) :-
+%@     user:e1(a),
+%@     no-(user:e2(_)).
+%@ true.
